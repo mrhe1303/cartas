e74e5682c855b8a3fa61fc2d2ff25a9c2cca1f55
mrhe1303 <mrhe1303@gmail.com>
2025-07-30 19:30:17 +0000

    Actualización 1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["heart", "spade", "club", "diamond"];
+
+  const valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+
+
+  const palosSimbolos = {
+
+    heart: "♥",
+    spade: "♠",
+    club: "♣",
+    diamond: "♦"
+
+  };
+
+  const randomPaloIndex = Math.floor(Math.random() * palos.length);
+  const randomValorIndex = Math.floor(Math.random() * valores.length);
+
+  const randomPalo = palos[randomPaloIndex];
+  const randomValor = valores[randomValorIndex];
+
+  const cartaContenedor = document.querySelector("#carta-contenedor");
+
+  cartaContenedor.className = "carta " + randomPalo;
+
+  document.querySelector(".carta-valor").textContent = randomValor;
+
+  const simbolos = document.querySelectorAll(".carta-simbolo");
+
+  simbolos.forEach(simbolo => {
+    simbolo.textContent = palosSimbolos[randomPalo];
+  });
+
+  console.log(`Generar carta: ${randomValor} de ${randomPalo}es`);
+
 };
